c0cd0c7f9688e612bc33ab7df41f9c720b09d3c0
Lucas Cordedro <lucascordero1997@gmail.com>
2025-06-12 18:17:17 -0300

    Change Results list to a ts file, update main page

---
+++ src/data/resultList.ts
@@ -0,0 +1,7 @@
+export const results = [
+    { date: "2025-05-31", opponent: "AMISTOSO", score: "CANCELADO", status: "Loss" },
+    { date: "2025-06-07", opponent: "Beber a Futuro", score: "2 - 1", status: "Loss" },
+    { date: "2025-06-14", opponent: "Sassone", score: "Próximamente...", status: "Proximo" },
+    //{ date: "2025-05-11", opponent: "Agronomia Central", score: "2 - 2", status: "Draw" },
+    //{ date: "2025-05-04", opponent: "Paternal Juniors", score: "4 - 0", status: "Win" },
+];
